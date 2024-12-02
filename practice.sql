da58748570eb7fd4d77a4afccb8b089d008e7315
sahil-tapkir <sahiltapkir07@gmail.com>
2024-12-02 17:47:04 +0530

    added book_shop database and created database books

--- practice.sql
+++ practice.sql
@@ -1,25 +1,25 @@
 -- Active: 1732949880220@@127.0.0.1@3306@practice1
-use practice1; 
- SELECT DATABASE();
-DROP TABLE cats;
-CREATE TABLE cats (
-    cat_id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    breed VARCHAR(100),
-    age INT,
-    PRIMARY KEY (cat_id)
-); 
+-- use practice1; 
+--  SELECT DATABASE();
+-- DROP TABLE cats;
+-- CREATE TABLE cats (
+--     cat_id INT AUTO_INCREMENT,
+--     name VARCHAR(100),
+--     breed VARCHAR(100),
+--     age INT,
+--     PRIMARY KEY (cat_id)
+-- ); 
 
-INSERT INTO cats(name, breed, age) 
-VALUES ('Ringo', 'Tabby', 4),
-       ('Cindy', 'Maine Coon', 10),
-       ('Dumbledore', 'Maine Coon', 11),
-       ('Egg', 'Persian', 4),
-       ('Misty', 'Tabby', 13),
-       ('George Michael', 'Ragdoll', 9),
-       ('Jackson', 'Sphynx', 7);
+-- INSERT INTO cats(name, breed, age) 
+-- VALUES ('Ringo', 'Tabby', 4),
+--        ('Cindy', 'Maine Coon', 10),
+--        ('Dumbledore', 'Maine Coon', 11),
+--        ('Egg', 'Persian', 4),
+--        ('Misty', 'Tabby', 13),
+--        ('George Michael', 'Ragdoll', 9),
+--        ('Jackson', 'Sphynx', 7);
 
-DESC cats;
+-- DESC cats;
 
 
 
@@ -63,58 +63,76 @@ DESC cats;
 
 --Super Crud Excercise
 
-CREATE DATABASE shirts_db;
+-- CREATE DATABASE shirts_db;
 
-USE shirts_db;
+-- USE shirts_db;
+-- SELECT DATABASE();
+
+-- CREATE TABLE shirts(
+--     shirt_id INT AUTO_INCREMENT PRIMARY KEY,
+--     article VARCHAR(30),
+--     color VARCHAR(30),
+--     shirt_size VARCHAR(30),
+--     last_worn INT
+-- );
+
+-- DESC shirts;
+
+-- INSERT INTO shirts(article,color,shirt_size,last_worn)
+-- VALUES
+-- ('t-shirt', 'white', 'S', 10),
+-- ('t-shirt', 'green', 'S', 200),
+-- ('polo shirt', 'black', 'M', 10),
+-- ('tank top', 'blue', 'S', 50),
+-- ('t-shirt', 'pink', 'S', 0),
+-- ('polo shirt', 'red', 'M', 5),
+-- ('tank top', 'white', 'S', 200),
+-- ('tank top', 'blue', 'M', 15);
+
+-- SELECT * FROM shirts;
+-- INSERT INTO shirts(article,color,shirt_size,last_worn)
+-- VALUES
+-- ('polo shirt','purple','M',50);
+-- SELECT * FROM shirts;
+-- SELECT article,color FROM shirts;
+
+-- SELECT article,color,shirt_size,last_worn FROM shirts WHERE shirt_size='M';
+
+-- SELECT * FROM shirts;
+
+-- UPDATE shirts SET last_worn=0 WHERE last_worn=15;
+
+-- SELECT * FROM shirts;
+
+-- UPDATE shirts SET shirt_size='XS' , color='off white' WHERE color='white';
+-- SELECT * FROM shirts;
+-- DELETE FROM shirts WHERE last_worn=200;
+-- SELECT * from shirts;
+-- DELETE FROM shirts WHERE article='tank top';
+
+-- SELECT * FROM shirts;
+-- DELETE FROM shirts;
+-- SELECT * FROM shirts;
+-- DESC shirts;
+
+-- DROP TABLE shirts;
+--  SELECT * FROM shirts;
+
+
+-- STRING FUNCTION
+CREATE DATABASE book_shop;
+USE book_shop;
+SELECT DATABASE();
+SHOW TABLES;
+SELECT * FROM books;
+DESC books;
+
 SELECT DATABASE();
 
-CREATE TABLE shirts(
-    shirt_id INT AUTO_INCREMENT PRIMARY KEY,
-    article VARCHAR(30),
-    color VARCHAR(30),
-    shirt_size VARCHAR(30),
-    last_worn INT
-);
+USE book_shop;
+SELECT DATABASE();
 
-DESC shirts;
+SELECT * FROM books;
 
-INSERT INTO shirts(article,color,shirt_size,last_worn)
-VALUES
-('t-shirt', 'white', 'S', 10),
-('t-shirt', 'green', 'S', 200),
-('polo shirt', 'black', 'M', 10),
-('tank top', 'blue', 'S', 50),
-('t-shirt', 'pink', 'S', 0),
-('polo shirt', 'red', 'M', 5),
-('tank top', 'white', 'S', 200),
-('tank top', 'blue', 'M', 15);
+DESC books;
 
-SELECT * FROM shirts;
-INSERT INTO shirts(article,color,shirt_size,last_worn)
-VALUES
-('polo shirt','purple','M',50);
-SELECT * FROM shirts;
-SELECT article,color FROM shirts;
-
-SELECT article,color,shirt_size,last_worn FROM shirts WHERE shirt_size='M';
-
-SELECT * FROM shirts;
-
-UPDATE shirts SET last_worn=0 WHERE last_worn=15;
-
-SELECT * FROM shirts;
-
-UPDATE shirts SET shirt_size='XS' , color='off white' WHERE color='white';
-SELECT * FROM shirts;
-DELETE FROM shirts WHERE last_worn=200;
-SELECT * from shirts;
-DELETE FROM shirts WHERE article='tank top';
-
-SELECT * FROM shirts;
-DELETE FROM shirts;
-SELECT * FROM shirts;
-DESC shirts;
-
-DROP TABLE shirts;
- SELECT * FROM shirts;
- 
