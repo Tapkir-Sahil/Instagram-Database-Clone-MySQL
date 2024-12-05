f50a765fd3fe3a747c0b82dc481b7e4bfdc20115
sahil-tapkir <sahiltapkir07@gmail.com>
2024-12-05 10:34:53 +0530

    completed refining selections

--- practice.sql
+++ practice.sql
@@ -95,21 +95,15 @@
 -- ('polo shirt','purple','M',50);
 -- SELECT * FROM shirts;
 -- SELECT article,color FROM shirts;
-
 -- SELECT article,color,shirt_size,last_worn FROM shirts WHERE shirt_size='M';
-
 -- SELECT * FROM shirts;
-
 -- UPDATE shirts SET last_worn=0 WHERE last_worn=15;
-
 -- SELECT * FROM shirts;
-
 -- UPDATE shirts SET shirt_size='XS' , color='off white' WHERE color='white';
 -- SELECT * FROM shirts;
 -- DELETE FROM shirts WHERE last_worn=200;
 -- SELECT * from shirts;
 -- DELETE FROM shirts WHERE article='tank top';
-
 -- SELECT * FROM shirts;
 -- DELETE FROM shirts;
 -- SELECT * FROM shirts;
@@ -118,7 +112,8 @@
 --  SELECT * FROM shirts;
 
 
--- STRING FUNCTION
+-- STRING FUNCTION:
+
 -- CREATE DATABASE book_shop;
 -- USE book_shop;
 -- SELECT DATABASE();
@@ -144,12 +139,12 @@
 
 -- #SUBSTRING
 
-use book_shop;
-SELECT DATABASE();
+-- use book_shop;
+-- SELECT DATABASE();
 -- SELECT SUBSTR(title,1,15)FROM books;
 -- SELECT SUBSTR(title,1,9)AS 'Short Title' FROM books;
 
--- #Combining Concat and Substring
+-- #COMBINING CONCAT AND SUBSTRING
 -- SELECT CONCAT(SUBSTR(title,1,9),'...')AS 'Short Title' FROM books;
 -- SELECT CONCAT(
 --     SUBSTR(author_fname,1,1),
@@ -192,7 +187,7 @@ SELECT DATABASE();
 --     UPPER(title)
 -- )AS 'Books' FROM books;
 
--- -- Other String Function:
+-- -- OTHER STRING FUNCTIONS:
 -- SELECT INSERT('Hello World',6,0,' There');
 -- SELECT INSERT(title,1,0,'HELLO ') FROM books;
 -- SELECT LEFT(title,5) FROM books;
@@ -204,40 +199,90 @@ SELECT DATABASE();
 
 -- String Function Excercise:
 
-SELECT REVERSE(
-    UCASE('Why does my cat look at me with such hatred?')
-);
-SELECT REPLACE
-(
-        CONCAT('I', ' ', 'Like', ' ', 'Cats'),
-        ' ',
-        '-'
-);
+-- SELECT REVERSE(
+--     UCASE('Why does my cat look at me with such hatred?')
+-- );
+-- SELECT REPLACE
+-- (
+--         CONCAT('I', ' ', 'Like', ' ', 'Cats'),
+--         ' ',
+--         '-'
+-- );
 
-use book_shop;
-SELECT DATABASE();
-SELECT REPLACE(title,' ','->')AS 'title' FROM books;
-SELECT * FROM books;
-SELECT author_lname AS 'Forwards',REVERSE(author_lname)AS 'Backwards' FROM books;
-SELECT UPPER(
-    CONCAT(author_fname,' ',author_lname)
-)AS 'Full Name In Caps' FROM books;
-SELECT CONCAT(
-    title,
-    ' Was Released In ',
-    released_year
-)AS Blurb FROM books;
-SELECT title,CHAR_LENGTH(title)AS 'Character Count' FROM books;
+-- use book_shop;
+-- SELECT DATABASE();
+-- SELECT REPLACE(title,' ','->')AS 'title' FROM books;
+-- SELECT * FROM books;
+-- SELECT author_lname AS 'Forwards',REVERSE(author_lname)AS 'Backwards' FROM books;
+-- SELECT UPPER(
+--     CONCAT(author_fname,' ',author_lname)
+-- )AS 'Full Name In Caps' FROM books;
+-- SELECT CONCAT(
+--     title,
+--     ' Was Released In ',
+--     released_year
+-- )AS Blurb FROM books;
+-- SELECT title,CHAR_LENGTH(title)AS 'Character Count' FROM books;
+-- SELECT CONCAT(LEFT(title,10),'...' )AS 'short title',
+-- CONCAT(author_lname,',',author_fname) AS author,
+-- CONCAT(stock_quantity,' in stock') AS quantity
+-- FROM books;
 
-SELECT CONCAT(LEFT(title,10),'...' )AS 'short title',
-CONCAT(author_lname,',',author_fname) AS author,
-CONCAT(stock_quantity,' in stock') AS quantity
-FROM books;
-
---Refining Seletions:
+--REFINING SELECTIONS:
 INSERT INTO books
     (title, author_fname, author_lname, released_year, stock_quantity, pages)
     VALUES ('10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
            ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
            ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
 SELECT * FROM books;
+USE book_shop;
+SELECT DATABASE();
+SELECT * FROM books;
+
+--DISTINCT 
+SELECT DISTINCT author_lname FROM books;
+SELECT DISTINCT CONCAT(author_fname,' ',author_lname )AS 'author fullname' FROM books;
+
+--ORDER BY:
+SELECT * FROM books ORDER BY author_lname;
+SELECT title,pages FROM books ORDER BY pages DESC;
+SELECT title , released_year FROM books ORDER BY released_year;
+SELECT author_fname,released_year,title
+FROM books 
+ORDER BY author_fname ,released_year DESC;
+
+--LIMIT:
+
+SELECT title,released_year FROM books
+ORDER BY released_year
+LIMIT 5;
+SELECT title,released_year FROM books
+ORDER BY released_year DESC
+LIMIT 3,10;
+SELECT title,released_year FROM books ORDER BY released_year DESC;
+
+--LIKE :
+
+SELECT  author_fname,title FROM books WHERE author_fname LIKE '%da%';
+SELECT author_fname, title FROM books WHERE author_fname LIKE '___';
+
+-- ESCAPING WILDDCARDS:
+SELECT title FROM books WHERE title LIKE '%\%%';
+SELECT title FROM books WHERE title LIKE '%\_%';
+
+--REFINING SELECTIONS EXCERCISE:
+
+SELECT title FROM books WHERE title LIKE '%stories%';
+SELECT title,pages FROM books ORDER BY pages DESC LIMIT 0,1;
+SELECT CONCAT(
+    title,' - ',released_year
+    ) AS 'summary'
+FROM books
+ORDER BY released_year DESC LIMIT 0,3;
+SELECT title,author_lname FROM books WHERE author_lname LIKE '% %';
+SELECT title,released_year,stock_quantity FROM books ORDER BY stock_quantity,title LIMIT 0,3;
+SELECT title,author_lname FROM books ORDER BY author_lname,title;
+
+SELECT UPPER(CONCAT('MY FAVORITE AUTHOR IS ',author_fname,' ',author_lname,' !'))
+FROM books 
+ORDER BY author_lname;
