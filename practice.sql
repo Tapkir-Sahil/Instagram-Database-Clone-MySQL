377846f12476a90d39621ec0e803d15c0fc2f367
sahil-tapkir <sahiltapkir07@gmail.com>
2024-12-06 19:00:10 +0530

    revisiting datatypes

--- practice.sql
+++ practice.sql
@@ -389,3 +389,31 @@ SELECT
 FROM books 
 GROUP BY released_year
 ORDER BY released_year;
+
+--REVISTING DATATYPES:
+--WORKING WITH DATA AND TIME
+
+CREATE TABLE people(
+    name VARCHAR(50),
+    birthdate DATE,
+    birthtime TIME,
+    birthdt DATETIME
+);
+
+DESC people;
+INSERT INTO people(name,birthdate,birthtime,birthdt)
+VALUES
+('Elton','2000-12-25','11:00:00','2000-12-25 11:00:00');
+
+SELECT * FROM people;
+
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Lulu', '1985-04-11', '9:45:10', '1985-04-11 9:45:10');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Juan', '2020-08-15', '23:59:00', '2020-08-15 23:59:00');
+
+SELECT * FROM people;
+
+DELETE FROM people WHERE name='juan';
+SELECT * FROM people;
