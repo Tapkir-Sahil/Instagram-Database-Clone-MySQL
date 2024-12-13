32ef667469c32c5ba9f429e1faa97ced671908a4
sahil-tapkir <sahiltapkir07@gmail.com>
2024-12-13 13:13:54 +0530

    Created table customers and orders and apply foriegn key to the order from customers

--- practice.sql
+++ practice.sql
@@ -643,3 +643,39 @@ ALTER TABLE companies
 CHANGE company_name company_UN VARCHAR(25);
 DESC companies;
 
+-- ONE TO MANY JOINS:
+-- Working With Foreign Key:
+
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
+
+ 
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+SELECT * from customers;
+ 
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
+
+SELECT * FROM orders;
